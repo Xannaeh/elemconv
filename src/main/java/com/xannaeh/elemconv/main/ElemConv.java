b77b9e91bf5301871f990dc0fc0240f68020ddc0
xannaeh <anabel99.anabel99@gmail.com>
2024-08-02 15:02:47 +0200

    Changed: Modified the creative tabs

--- src/main/java/com/xannaeh/elemconv/main/ElemConv.java
+++ src/main/java/com/xannaeh/elemconv/main/ElemConv.java
@@ -3,25 +3,31 @@ package com.xannaeh.elemconv.main;
 import com.xannaeh.elemconv.block.ModBlocks;
 import com.xannaeh.elemconv.item.ModItems;
 import com.xannaeh.elemconv.ui.ModTabs;
+import net.neoforged.fml.ModList;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import org.slf4j.Logger;
-
-import com.mojang.logging.LogUtils;
 
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
+import org.slf4j.LoggerFactory;
 
-// The value here should match an entry in the META-INF/neoforge.mods.toml file
+
 @Mod(ElemConv.MODID)
 public class ElemConv {
-    // Define mod id in a common place for everything to reference
     public static final String MODID = "elemconv";
-    // Directly reference a slf4j logger
-    private static final Logger LOGGER = LogUtils.getLogger();
+    private static final Logger logger = LoggerFactory.getLogger(ElemConv.class);
 
     public ElemConv(IEventBus modEventBus, ModContainer modContainer) {
         ModItems.ITEMS.register(modEventBus);
         ModBlocks.BLOCKS.register(modEventBus);
         ModTabs.CREATIVE_TABS.register(modEventBus);
+        modEventBus.addListener(FMLClientSetupEvent.class, (fmlClientSetupEvent -> {
+            fmlClientSetupEvent.enqueueWork(() -> {
+                ModList.get().getModContainerById(MODID).ifPresent(modContainer1 -> {
+                    logger.info("Hello from Elemental Convergence, using version: {}", modContainer.getModInfo().getVersion());
+                });
+            });
+        }));
     }
 }
--- src/main/java/com/xannaeh/elemconv/ui/ModTabs.java
+++ src/main/java/com/xannaeh/elemconv/ui/ModTabs.java
@@ -9,40 +9,71 @@ import net.minecraft.world.item.CreativeModeTabs;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
+
 import static com.xannaeh.elemconv.main.ElemConv.MODID;
 
 public class ModTabs {
     public static final DeferredRegister<CreativeModeTab> CREATIVE_TABS =
             DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
 
-    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> ELEM_CONV_ITEMS =
-            CREATIVE_TABS.register("elemconvitems", () ->
+    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> ELEM_CONV_LIGHT =
+            CREATIVE_TABS.register("elemconv_light", () ->
                     CreativeModeTab.builder()
-                            .title(Component.translatable("itemGroup.elemconv"))
+                            .title(Component.translatable("lightGroup.elemconv"))
                             .withTabsBefore(CreativeModeTabs.COMBAT)
                             .icon(() -> ModItems.RAW_DARKNESS.get().getDefaultInstance()) //TODO: Change for real icon later
-                            .displayItems(((parameters, output) -> {
-                                output.accept(ModItems.RAW_DARKNESS.get());
-                                output.accept(ModItems.DARKNESS_FRUIT.get());
-                                output.accept(ModItems.DARKNESS_FRUIT_DRIED.get());
-                                output.accept(ModItems.DARKNESS_INGOT.get());
-                                output.accept(ModItems.DARKNESS_ESSENCE.get());
+                            .displayItems(((itemDisplay, output) -> {
+                                output.accept(ModBlocks.LIGHT_ORE.get());
                             }))
                             .build());
 
-    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> ELEM_CONV_BLOCK =
-            CREATIVE_TABS.register("elemconvblocks", () ->
+    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> ELEM_CONV_DARKNESS =
+            CREATIVE_TABS.register("elemconv_darkness", () ->
                     CreativeModeTab.builder()
-                            .title(Component.translatable("blockGroup.elemconv"))
-                            .withTabsBefore(ELEM_CONV_ITEMS.getKey())
+                            .title(Component.translatable("darknessGroup.elemconv"))
+                            .withTabsBefore(ELEM_CONV_LIGHT.getKey())
                             .icon(() -> ModItems.DARKNESS_ORE.get().getDefaultInstance()) //TODO: Change for real icon later
                             .displayItems(((parameters, output) -> {
                                 output.accept(ModBlocks.DARKNESS_ORE.get());
-                                output.accept(ModBlocks.LIGHT_ORE.get());
+                                output.accept(ModItems.RAW_DARKNESS.get());
+                                output.accept(ModItems.DARKNESS_INGOT.get());
                                 output.accept(ModBlocks.DARKNESS_TREE_LOG.get());
                                 output.accept(ModBlocks.DARKNESS_TREE_PLANKS.get());
                                 output.accept(ModBlocks.DARKNESS_TREE_SLAB.get());
                                 output.accept(ModBlocks.DARKNESS_FLOWER.get());
+                                output.accept(ModItems.DARKNESS_FRUIT.get());
+                                output.accept(ModItems.DARKNESS_FRUIT_DRIED.get());
+                                output.accept(ModItems.DARKNESS_ESSENCE.get());
+                            }))
+                            .build());
+
+    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> ELEM_CONV_ARCANE =
+            CREATIVE_TABS.register("elemconv_arcane", () ->
+                    CreativeModeTab.builder()
+                            .title(Component.translatable("arcaneGroup.elemconv"))
+                            .withTabsBefore(ELEM_CONV_DARKNESS.getKey())
+                            .icon(() -> ModItems.RAW_DARKNESS.get().getDefaultInstance()) //TODO: Change for real icon later
+                            .displayItems(((itemDisplay, output) -> {
+                            }))
+                            .build());
+
+    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> ELEM_CONV_DUAL =
+            CREATIVE_TABS.register("elemconv_dual", () ->
+                    CreativeModeTab.builder()
+                            .title(Component.translatable("dualGroup.elemconv"))
+                            .withTabsBefore(ELEM_CONV_ARCANE.getKey())
+                            .icon(() -> ModItems.RAW_DARKNESS.get().getDefaultInstance()) //TODO: Change for real icon later
+                            .displayItems(((itemDisplay, output) -> {
+                            }))
+                            .build());
+
+    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> ELEM_CONV_MISC =
+            CREATIVE_TABS.register("elemconv_misc", () ->
+                    CreativeModeTab.builder()
+                            .title(Component.translatable("miscGroup.elemconv"))
+                            .withTabsBefore(ELEM_CONV_DUAL.getKey())
+                            .icon(() -> ModItems.RAW_DARKNESS.get().getDefaultInstance()) //TODO: Change for real icon later
+                            .displayItems(((itemDisplay, output) -> {
                             }))
                             .build());
 
